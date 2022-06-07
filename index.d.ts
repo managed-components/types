52bab38ad9dda7b701282ce72c005aaaf7c62e56
Ruskin Constant <6400000+jonnyparris@users.noreply.github.com>
2022-06-07 16:00:51 +0100

    add explicit event types for event listener methods

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,76 @@
 interface ComponentSettings {
-  [key: string]: any;
+  [key: string]: any
 }
 
 interface ClientSetOptions {
-  readonly scope?: "page" | "session" | "infinite";
-  readonly expiry?: Date | number | null;
+  readonly scope?: 'page' | 'session' | 'infinite'
+  readonly expiry?: Date | number | null
 }
 
 type EmbedCallback = (context: {
-  parameters: { [k: string]: unknown };
-}) => Promise<string>;
+  parameters: { [k: string]: unknown }
+}) => Promise<string>
 
-type WidgetCallback = () => Promise<string>;
+type WidgetCallback = () => Promise<string>
 
 interface MCEvent extends Event {
-  readonly name?: string;
-  readonly payload: any;
-  client: Client;
-  readonly type: string;
+  readonly name?: string
+  readonly payload: any
+  client: Client
+  readonly type: string
 }
 
 interface MCEventListener {
-  (event: MCEvent): void;
+  (event: MCEvent): void
 }
 
-interface Manager {
-  readonly name: string;
+type ManagerEventType = 'clientcreated' | 'pageview' | 'ecommerce'
 
-  addEventListener(type: string, callback: MCEventListener): void;
-  createEventListener(type: string, callback: MCEventListener): void;
-  get(key: string): string;
-  set(key: string, value: any): void;
-  route(path: string, callback: (request: Request | any) => Response): string;
-  proxy(path: string, target: string): string;
-  serve(path: string, target: string): string;
-  useCache(key: string, callback: Function, expiry?: number): any;
-  invalidateCache(key: string): void;
-  registerEmbed(name: string, callback: EmbedCallback): void;
-  registerWidget(callback: WidgetCallback): void;
+type ClientEventType =
+  | 'event'
+  | 'mouseup'
+  | 'mousedown'
+  | 'mousemove'
+  | 'visiblityChange'
+  | 'historyChange'
+  | 'pageHide'
+  | 'pageShow'
+  | 'resize'
+  | 'scroll'
+  | 'resourcePerformanceEntry'
+
+interface Manager {
+  readonly name: string
+
+  addEventListener(type: ManagerEventType, callback: MCEventListener): void
+  createEventListener(type: ClientEventType, callback: MCEventListener): void
+  get(key: string): string
+  set(key: string, value: any): void
+  route(path: string, callback: (request: Request | any) => Response): string
+  proxy(path: string, target: string): string
+  serve(path: string, target: string): string
+  useCache(key: string, callback: Function, expiry?: number): any
+  invalidateCache(key: string): void
+  registerEmbed(name: string, callback: EmbedCallback): void
+  registerWidget(callback: WidgetCallback): void
 }
 
 interface Client {
-  readonly emitter: string;
-  readonly userAgent: string;
-  readonly language: string;
-  readonly referer: string;
-  readonly ip: string;
-  readonly title?: string;
-  readonly timestamp?: number;
-  readonly url: URL;
+  readonly emitter: string
+  readonly userAgent: string
+  readonly language: string
+  readonly referer: string
+  readonly ip: string
+  readonly title?: string
+  readonly timestamp?: number
+  readonly url: URL
 
-  fetch(resource: string, settings?: RequestInit): void;
-  execute(code: string): void;
-  return(value: unknown): void;
-  set(key: string, value?: string | null, opts?: ClientSetOptions): void;
-  get(key: string): string;
-  attachEvent(event: string): void;
+  fetch(resource: string, settings?: RequestInit): void
+  execute(code: string): void
+  return(value: unknown): void
+  set(key: string, value?: string | null, opts?: ClientSetOptions): void
+  get(key: string): string
+  attachEvent(event: ClientEventType): void
 }
 
 export {
@@ -67,4 +82,4 @@ export {
   MCEventListener,
   Manager,
   Client,
-};
+}
